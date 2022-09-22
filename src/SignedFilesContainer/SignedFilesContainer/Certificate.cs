f49dde1750f7d90c772153783c304291def8681c
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2022-09-22 18:39:30 +0300

    CreateSelfSignedX509Certificate

---
+++ src/SignedFilesContainer/SignedFilesContainer/Certificate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SignedFilesContainer
+{
+    /// <summary>
+    /// A wrapper for X509 certificate and its RSA public key providing simple API
+    /// not requiring deep cryptography knownledge.
+    /// </summary>
+    public class Certificate
+    {
+        private readonly X509Certificate2? _certificate;
+        private readonly string? _password;
+        private byte[]? _RSAPublicKey;
+
+        public Certificate(X509Certificate2 certificate, string? password)
+        {
+            _certificate = certificate ?? throw new ArgumentNullException(nameof(certificate));
+            _password = password;
+        }
+
+        private Certificate(byte[]? pfxCertificateBytes, byte[]? publicKeyBytes, string? password)
+        {
+            if (pfxCertificateBytes is null && publicKeyBytes is null)
+                throw new ArgumentException($"One of {nameof(pfxCertificateBytes)} | {nameof(publicKeyBytes)} must be not null.");
+
+            if (pfxCertificateBytes is not null)
+            {
+                _certificate = new X509Certificate2(pfxCertificateBytes, password);
+            }
+
+            if (publicKeyBytes is not null)
+            {
+                _RSAPublicKey = publicKeyBytes;
+            }
+
+            _password = password;
+        }
+
+        public static Certificate FromPfxBytes(byte[] pfxCertificateBytes, string? password)
+        {
+            if (pfxCertificateBytes is null)
+                throw new ArgumentNullException(nameof(pfxCertificateBytes));
+
+            return new(pfxCertificateBytes, null, password);
+        }
+
+        public static Certificate FromRSAPublicKeyBytes(byte[] publicKeyBytes)
+        {
+            if (publicKeyBytes is null)
+                throw new ArgumentNullException(nameof(publicKeyBytes));
+
+            return new(null, publicKeyBytes, null);
+        }
+
+        public bool HasCertificate => _certificate is not null;
+
+        public bool HasPublicKey => _RSAPublicKey is not null || _certificate is not null;
+
+        /// <summary>
+        /// Gets certificate bytes for pfx format. You can save the to file like "MyCertificate.pfx".
+        /// </summary>
+        /// <returns>Bytes in pfx format</returns>
+        public ReadOnlySpan<byte> GetPfxBytes()
+        {
+            if (_certificate is null)
+                throw new InvalidOperationException("No certificate");
+
+            byte[] certBytes = _certificate.Export(X509ContentType.Pfx, _password);
+            return certBytes;
+        }
+
+        /// <summary>
+        /// Gets bytes for RSA public key.
+        /// </summary>
+        /// <returns>RSA public key bytes</returns>
+        public ReadOnlySpan<byte> GetRSAPublicKeyBytes()
+        {
+            if (_RSAPublicKey is not null)
+                return _RSAPublicKey;
+
+            RSA? rsaPublicKey = _certificate!.PublicKey.GetRSAPublicKey();
+            if (rsaPublicKey == null)
+                throw new InvalidOperationException("Failed to get RSA Public Key from certificate.");
+
+            _RSAPublicKey = rsaPublicKey.ExportSubjectPublicKeyInfo();
+            return _RSAPublicKey;
+        }
+    }
+}
---
+++ src/SignedFilesContainer/SignedFilesContainer/CertificateHelpers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SignedFilesContainer
+{
+    /// <summary>
+    /// Static helper methods.
+    /// </summary>
+    public static class CertificateHelpers
+    {
+        public static Certificate CreateSelfSignedX509Certificate(string certificateName, string? password, string? dnsName = null)
+        {
+            var X509Certificate = CreateSelfSignedServerCertificate(certificateName, password, dnsName);
+            return new Certificate(X509Certificate, password);
+        }
+
+        // https://creativecommons.org/licenses/by-sa/4.0/
+        // https://stackoverflow.com/a/50138133/1175698
+        public static X509Certificate2 CreateSelfSignedServerCertificate(string certificateName, string? password, string? dnsName = null)
+        {
+            if (string.IsNullOrEmpty(certificateName))
+                throw new ArgumentException($"{certificateName} is required.");
+
+            SubjectAlternativeNameBuilder sanBuilder = new();
+            sanBuilder.AddIpAddress(IPAddress.Loopback);
+            sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
+            sanBuilder.AddDnsName("localhost");
+            if (!string.IsNullOrEmpty(dnsName))
+            {
+                sanBuilder.AddDnsName(dnsName);
+            }
+
+            X500DistinguishedName distinguishedName = new(distinguishedName: $"CN={certificateName}");
+
+            using RSA rsa = RSA.Create(2048);
+
+            var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+            request.CertificateExtensions.Add(
+                new X509KeyUsageExtension(
+                    X509KeyUsageFlags.DataEncipherment |
+                    X509KeyUsageFlags.KeyEncipherment |
+                    X509KeyUsageFlags.DigitalSignature,
+                    critical: false));
+
+            request.CertificateExtensions.Add(
+               new X509EnhancedKeyUsageExtension(
+                   new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
+
+            request.CertificateExtensions.Add(sanBuilder.Build());
+
+            var certificate = request.CreateSelfSigned(
+                notBefore: new DateTimeOffset(DateTime.UtcNow.AddDays(-1)),
+                notAfter: new DateTimeOffset(DateTime.UtcNow.AddYears(10)));
+            // certificate.FriendlyName = certificateName; // FriendlyName used only under Windows
+
+            byte[] bytes = certificate.Export(X509ContentType.Pfx, password);
+            return new X509Certificate2(bytes, password, X509KeyStorageFlags.Exportable);
+        }
+    }
+}
