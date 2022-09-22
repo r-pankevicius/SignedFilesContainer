7eb49aef860c4009f9abaddc74c713892f9c6710
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2022-09-22 22:08:26 +0300

    Noop Spectre.Console.Cli

---
+++ src/SignedFilesContainer/SignedFilesContainerCLI/Commands/GenerateCertCommand.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console.Cli;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignedFilesContainerCLI.Commands
+{
+    /// <summary>
+    /// Generate self signed certificate.
+    /// </summary>
+    internal class GenerateCertCommand : Command<GenerateCertCommand.Settings>
+    {
+        public class Settings : CommandSettings
+        {
+        }
+
+        public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
+        {
+            return 0;
+        }
+    }
+}
--- src/SignedFilesContainer/SignedFilesContainerCLI/Program.cs
+++ src/SignedFilesContainer/SignedFilesContainerCLI/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using SignedFilesContainerCLI.Commands;
+using Spectre.Console.Cli; // don't uprade to a newer version of Spectre.Console otherwise you'll get an error here. https://www.nuget.org/packages?q=Spectre.Console.Cli is in preview 0.8
+using System;
+using System.Reflection;
 
 namespace SignedFilesContainerCLI
 {
     internal static class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{nameof(SignedFilesContainerCLI)} {Assembly.GetExecutingAssembly().GetName().Version}");
+
+            var x = new Spectre.Console.Calendar(DateTime.Now);
+            var cliApp = new CommandApp<GenerateCertCommand>();
+            cliApp.Configure(config =>
+            {
+            });
+
+            return cliApp.Run(args);
         }
     }
 }
